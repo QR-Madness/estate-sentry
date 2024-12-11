2696e2cafa68b077bdc06d6f9ec9ad3116911365
JP <developerjp@pm.me>
2024-12-11 15:56:18 -0500

    Add .idea inspection profile, requirements, and sensor endpoints.
    Added a new `.idea/inspectionProfiles/Project_Default.xml` for project inspection settings and updated IntelliJ configurations. Introduced a `requirements.txt` file defining project dependencies. Extended `sensors.py` with new endpoints and docstrings to describe sensor-related functionalities.

--- estate-sentry-api/sensors.py
+++ estate-sentry-api/sensors.py
@@ -5,8 +5,12 @@ from flask import Blueprint, request
 
 auth_blueprint = Blueprint('EstateSentry:API:Sensors', __name__)
 
+
 @auth_blueprint.route('/sensor', methods=['GET', 'POST'])
 def sensor():
+    """
+    Retrieves info about a registered sensor.
+    """
     if request.method == 'GET':
         # TODO sensor data requested
         pass
@@ -16,6 +20,22 @@ def sensor():
         # TODO create sensor
         pass
 
+
 @auth_blueprint.route('/cctv/<camera_id>', methods=['GET'], defaults={'camera_id': None})
 def cctv(camera_id: str):
+    """
+    View a CCTV feed of a camera sensor.
+    :param camera_id: Sensor ID of camera.
+    """
     pass
+
+
+@auth_blueprint.route('/sensors', methods=['GET'])
+def sensors():
+    """
+    Retrieves info about current and available sensors.
+    """
+    # TODO get list of registered sensors
+    if request.json.include_unregistered_sensors:
+        # TODO get list of available unregistered sensors
+        pass
